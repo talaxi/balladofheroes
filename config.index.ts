04443129cd04599c46879f4bcb182691a28eafb9
talaxi <jaredbeaudrot@gmail.com>
2022-12-10 21:22:18 -0500

    fix prod version

---
+++ config.index.ts
@@ -0,0 +1,18 @@
+import {writeFile} from 'fs';
+
+import {name, version} from './package.json';
+
+const targetPath = 'src/environments/environment.prod.ts';
+
+const envConfigFile = `export const environment = {
+    production: true,
+    devEnvironment: false,
+    CODEREDEMPTIONSECRET: '${process.env.CODEREDEMPTIONSECRET}'  
+};
+`;
+
+writeFile(targetPath, envConfigFile, 'utf8', (err) => {
+  if (err) {
+    return console.log(err);
+  }
+});
