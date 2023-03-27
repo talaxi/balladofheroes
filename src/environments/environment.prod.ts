0df470f8ea6c5e7622daf7125a0dbfc64c2ae1bf
talaxi <jaredbeaudrot@gmail.com>
2023-03-27 19:55:25 -0400

    remove env prod

--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-    production: true,
-    staging: true,
-    devEnvironment: true,
-    CODEREDEMPTIONSECRET: 'MpvfJ6oRq8fItPw6kAvqamqllYn5rUTq',
-    STRIPESECRET: 'sk_test_51LmnxHDINqQNy3RQvtGFiTEHhdWOFAGlF81JnRPXyyjJ7s33f2qmmp08a2hDKPRy6ig2z3pngmNIC3HXcLH3qZq000QyxyDIRw',
-    PATREONCLIENTID: 'Twq8Ur3aDNI9o2t2FOewFCbXwCIl41YSORM_jFaPYU5N2ZX5BXpJi0NxoEWIxYW3',
-    PATREONCLIENTSECRET: '0A3dtH07czRezrsaqsts5uu6_vO5liSDyGYl9b8rT8JCg1oAusV0Q1VgSQCl9vYg'
-};
